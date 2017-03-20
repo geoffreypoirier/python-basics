71d08d67dd8514edd1834739b2c4850787d4ae56
Geoffrey Poirier <geoffreypoirier@gmail.com>
2017-03-19 23:09:58 -0600

    added `read` example; added comments, refs, etc; cleanup;

--- sqlite-basics.py
+++ sqlite-basics.py
@@ -1,8 +1,26 @@
+# I used SQLite on an old project and
+# want to organize the data collected
+# over the last five years.
+
+
+# ---
+# Links
+#
+# https://docs.python.org/3/library/sqlite3.html
+
+
+# ---
+# Imports
+
 import os
 import sqlite3
 import time
 import datetime
 import random
+
+
+# ---
+# Init
 
 databasePath = 'sqlite'
 databaseName = 'example.db'
@@ -12,25 +30,21 @@ exampleDirExists = os.path.exists(databasePath)
 if not exampleDirExists:
     os.mkdir(databasePath)
 
+
 conn = sqlite3.connect(databasePath + '/' + databaseName)
 cursor = conn.cursor()
 
 
 def create_table():
-    tableStatement = 'CREATE TABLE IF NOT EXISTS exampleTable(unix REAL, datestamp TEXT, keyword TEXT, value REAL)'
-    cursor.execute(tableStatement)
+    sqlStatement = 'CREATE TABLE IF NOT EXISTS exampleTable(unix REAL, datestamp TEXT, keyword TEXT, value REAL)'
+    cursor.execute(sqlStatement)
 
 
-def seed_db():
-    dataStatement = "INSERT INTO exampleTable VALUES(12444124, '2017-03-19', 'Python', 8)"
-    conn.commit()
-    # cursor.close()
-    # conn.close()
-
-
+# ---
+# Create
 # dummy data at the moment
-def create_record():
 
+def create_record():
     unix = int(time.time())
     date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))  # copy/paste from Sentdex
     keyword = 'avast, matey'
@@ -42,15 +56,31 @@ def create_record():
     conn.commit()
 
 
-# run the basic functions
+# ---
+# Read
+
+def read_all_records():
+    sqlStatement = "SELECT * FROM exampleTable"
+    cursor.execute(sqlStatement)
+    result = cursor.fetchall()
+    print(result)
+    for row in result:
+        print(row)
+
+    # WHERE, ORDER BY and all that other stuff ala carte
+
+
+# ---
+# Run the basic functions
 
 create_table()
 
-seed_db()
-
+# seed the db
 for i in range(25):
     create_record()
     time.sleep(0.5)  # for datestamp differences
+
+read_all_records()
 
 
 # close db connection
